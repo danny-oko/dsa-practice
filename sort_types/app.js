48182d202c63a35fe8fa6b8219c54364c24427e3
danny <dnioko0412@gmail.com>
2025-12-18 09:09:28 +0800

    hello wordl

---
+++ sort_types/app.js
@@ -0,0 +1,18 @@
+const nums = [3, 1, 2, 4];
+// move the even number in the beginning of the array
+const n = nums.length - 1;
+
+for (let i = 0; i < n; i++) {
+  if (i % 2 === 0) {
+    arr.slice();
+    
+  }
+  console.log(nums);
+}
+//  for (let j = i + 1; j < n; j++) {
+//     if (nums[j] % 2 === 0 && nums[i] % 2 !== 0) {
+
+//     }
+//     // else if (nums[i] % 2 === 0 && nums[j] % 2 !== 0) {
+//     // }
+//   }
--- sort_types/counting-sort/app.js
+++ sort_types/counting-sort/app.js
@@ -13,22 +13,27 @@
 // sortedArr = countingSort(unsortedArr)
 // print("Sorted array:", sortedArr)
 
-function countingSort(arr) {
-  if (!arr || arr.length === 0) return arr;
+function countingSort(arr) {  if (!arr || arr.length === 0) return arr;
   const maxVal = Math.max(...arr);
+
   const count = new Array(maxVal + 1).fill(0);
+
   for (const num of arr) {
     count[num] += 1;
   }
   arr.length = 0;
+
   for (let num = 0; num < count.length; num++) {
     const freq = count[num];
+
     for (let i = 0; i < freq; i++) {
       arr.push(num);
     }
   }
   return arr;
 }
+
 const unsortedArr = [4, 2, 2, 6, 3, 3, 1, 6, 5, 2, 3];
 const sortedArr = countingSort(unsortedArr);
+
 console.log("sorted array: ", sortedArr);
